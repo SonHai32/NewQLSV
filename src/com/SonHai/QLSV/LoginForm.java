00a19e4bd0be3b4b79d23633f729fe249e7379f7
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-05-27 12:29:40 +0700

    Tam On

--- src/com/SonHai/QLSV/LoginForm.java
+++ src/com/SonHai/QLSV/LoginForm.java
@@ -1,55 +1,70 @@
 package com.SonHai.QLSV;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginForm extends JFrame{
+public class LoginForm extends JFrame implements ActionListener {
+
     private JPanel loginPanel ;
+    private JPanel btnPanel;
     private JLabel usernameLabel ;
     private JLabel passwordLabel ;
     private JTextField usernameTextField ;
     private JPasswordField passwordField;
-    private JButton btnOK;
+    private JButton btnLogin;
 
     public void setUI(){
-        usernameLabel = new JLabel("Username",JLabel.RIGHT);
-        passwordLabel = new JLabel("Password",JLabel.RIGHT);
+        btnPanel = new JPanel();
+        btnLogin = new JButton("LOGIN");
+
+        usernameLabel = new JLabel("Username",JLabel.CENTER);
+        passwordLabel = new JLabel("Password",JLabel.CENTER);
 
         usernameLabel.setLabelFor(usernameTextField);
         passwordLabel.setLabelFor(passwordField);
 
         usernameTextField = new JTextField(20);
         passwordField = new JPasswordField(20);
+
         passwordField.setEchoChar('*');
 
-        btnOK = new JButton("OK");
+        usernameTextField.setBackground(Color.cyan);
+        usernameTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        passwordField.setBackground(Color.cyan);
+        passwordField.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+
+        btnPanel.setSize(100,20);
+        btnPanel.add(btnLogin);
 
         loginPanel = new JPanel();
-        loginPanel.setLayout(new GridLayout(3,2,5,5));
-        loginPanel.setSize(300,150);
+        loginPanel.setLayout(new GridLayout(2,2 ,5,5));
+        loginPanel.setSize(300,50);
         loginPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         loginPanel.add(usernameLabel);
         loginPanel.add(usernameTextField);
         loginPanel.add(passwordLabel);
         loginPanel.add(passwordField);
-        loginPanel.add(btnOK);
+        //loginPanel.add(btnLogin);
 
-        this.getRootPane().setBorder(BorderFactory.createMatteBorder(10,10,10,10,Color.GREEN));
-        this.add(loginPanel);
-        this.setSize(350,200);
-       // this.pack();
+
+        this.setLayout(new GridLayout(2,1));
+        this.getRootPane().setBorder(BorderFactory.createMatteBorder(6,6,6,6,Color.GREEN));
+        this.add(loginPanel,BorderLayout.CENTER);
+        this.add(btnPanel,BorderLayout.CENTER);
+        this.setSize(350,150);
+        this.pack();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        System.out.println(usernameLabel.getSize());
+        //System.out.println(btnOK.getBounds());
 
+    }
 
-
-
-
-
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
