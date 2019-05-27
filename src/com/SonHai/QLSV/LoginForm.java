9684ecace81de3b915e679dd3a1e8aaa069437d5
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-05-27 14:01:34 +0700

    Clean code and add ImageIcon

--- src/com/SonHai/QLSV/LoginForm.java
+++ src/com/SonHai/QLSV/LoginForm.java
@@ -13,17 +13,23 @@ public class LoginForm extends JFrame implements ActionListener {
     private JTextField usernameTextField ;
     private JPasswordField passwordField;
     private JButton btnLogin;
-    private ImageIcon errorIcon;
+    private ImageIcon errorIcon,userIcon,passIcon,frameIcon;
+    private Image frameImage;
 
     public void setUI(){
+
         errorIcon = new ImageIcon("src/image/ERROR.png");
+        userIcon = new ImageIcon("src/image/user_16px.png");
+        passIcon = new ImageIcon("src/image/password_16px.png");
+        frameIcon = new ImageIcon("src/image/enter_2_24px.png");
+        frameImage = frameIcon.getImage();
 
         btnPanel = new JPanel();
         btnLogin = new JButton("LOGIN");
         btnLogin.addActionListener(this);
 
-        usernameLabel = new JLabel("Username",JLabel.CENTER);
-        passwordLabel = new JLabel("Password",JLabel.CENTER);
+        usernameLabel = new JLabel("Username",userIcon,JLabel.CENTER);
+        passwordLabel = new JLabel("Password",passIcon,JLabel.CENTER);
 
         usernameLabel.setLabelFor(usernameTextField);
         passwordLabel.setLabelFor(passwordField);
@@ -36,31 +42,31 @@ public class LoginForm extends JFrame implements ActionListener {
         passwordField.setBackground(Color.cyan);
         passwordField.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
 
-        btnPanel.setSize(100,20);
+        btnPanel.setBackground(new Color(0,0,0,25));
         btnPanel.add(btnLogin);
 
         loginPanel = new JPanel();
+        loginPanel.setOpaque(true);
+        loginPanel.setBackground(new Color(0,0,0,25));
         loginPanel.setLayout(new GridLayout(2,2 ,5,5));
-        loginPanel.setSize(300,50);
+        //loginPanel.setSize(300,30);
         loginPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         loginPanel.add(usernameLabel);
         loginPanel.add(usernameTextField);
         loginPanel.add(passwordLabel);
         loginPanel.add(passwordField);
-        //loginPanel.add(btnLogin);
 
-
+        this.setTitle("Login to QLSV");
+        this.setIconImage(frameImage);
         this.setLayout(new GridLayout(2,1));
         this.getRootPane().setBorder(BorderFactory.createMatteBorder(6,6,6,6,Color.GREEN));
         this.add(loginPanel,BorderLayout.CENTER);
         this.add(btnPanel,BorderLayout.CENTER);
-        this.setSize(350,150);
         this.pack();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        System.out.println(errorIcon.getIconWidth()+"x"+errorIcon.getIconHeight());
 
     }
 
@@ -70,14 +76,12 @@ public class LoginForm extends JFrame implements ActionListener {
         long password = passwordField.getText().hashCode();
 
         if(username == -1943733835 && password == 948877636){
-            JOptionPane.showMessageDialog(this,"Done");
+            JOptionPane.showMessageDialog(this,"Done");//Change after
         }
         else{
 
             JOptionPane.showMessageDialog(this,"Password Incorrect","Login Fail",JOptionPane.ERROR_MESSAGE,errorIcon);
         }
-
-
 
     }
 }
