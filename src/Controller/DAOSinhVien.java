3131e96973134ce38ae8ba64f0597ec0e17e48cd
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-06-03 14:23:31 +0700

    create add+delete+update data SinhVien Method

--- src/Controller/DAOSinhVien.java
+++ src/Controller/DAOSinhVien.java
@@ -1,16 +1,76 @@
 package Controller;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import Model.SinhVien;
 public class DAOSinhVien extends DAO{
 	private ArrayList<SinhVien> listSinhVien = new ArrayList<SinhVien>();
-	
+
+	public boolean addSinhVien(Model.SinhVien x) {
+		String sql = ("INSERT INTO sinhvien(masv , hotensv , ngaysinh , phai , noisinh , malop , makh)"
+				+ "VALUES(?,?,?,?,?,?,?)");
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, x.getMaSinhVien());
+			ps.setString(2, x.getHoTen());
+			ps.setString(3, x.getNgaySinh());
+			ps.setString(4, x.getGioiTinh());
+			ps.setString(5, x.getNoiSinh());
+			ps.setString(6, x.getMaLop());
+			ps.setString(7, x.getMaKhoa());
+
+			return ps.executeUpdate() > 0 ;
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean updateSinhVien(Model.SinhVien x ) {
+		String sql = "UPDATE sinhvien "
+				+ "set masv = ? ,"
+				+ "hotensv = ? ,"
+				+ "ngaysinh = ? ,"
+				+ "gioitinh = ? ,"
+				+ "noisinh = ? ,"
+				+ "malop = ? ,"
+				+ "makh = ? , ";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, x.getMaSinhVien());
+			ps.setString(2, x.getHoTen());
+			ps.setString(3, x.getNgaySinh());
+			ps.setString(4, x.getGioiTinh());
+			ps.setString(5, x.getNoiSinh());
+			ps.setString(6, x.getMaLop());
+			ps.setString(6, x.getMaKhoa());
+
+			return ps.executeUpdate() > 0 ;
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public boolean deleteSinhVien(Model.SinhVien x) {
+		try {
+			PreparedStatement ps = con.prepareStatement("DELETE FROM sinhvien WHERE id = " + x.getID());
+
+			return ps.executeUpdate() > 0 ;
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return false;
+	}
+
 	public ArrayList<SinhVien> getListSinhVien(String sql) {
 		try {
 			Statement sta = con.createStatement();
 			ResultSet rs = sta.executeQuery(sql);
-			
+
 			while(rs.next()) {
 				SinhVien x = new SinhVien();
 				x.setID(rs.getString("id"));
@@ -26,9 +86,13 @@ public class DAOSinhVien extends DAO{
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}
-		
-		
+
+
 		return listSinhVien;
 	}
-	
+
+
+
+
 }
+
